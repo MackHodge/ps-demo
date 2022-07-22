1f24325a11475803dd6e811d42e10f180288131e
mackhodge <macklony@hotmail.com>
2022-07-22 12:31:58 +0200

    angular form new course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserSettingsFormsComponent } from './user-settings-forms/user-settings-forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSettingsFormsComponent
   ],
   imports: [
     BrowserModule
